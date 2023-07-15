93734cba081fc9103a19170e6a2154af637675b2
Mrck0s <110942627+Mrck0s@users.noreply.github.com>
2023-07-15 20:29:13 -0300

    maestro-objetos

---
+++ maestro-objetos/Human.java
@@ -0,0 +1,17 @@
+
+public class Human {
+  protected int strength = 3;
+  protected int intelligence = 3;
+  protected int stealth = 3;
+  protected int health = 100;
+
+  public Human(){
+
+  }
+
+  public Integer attack(Human target){
+    target.health -= this.strength;
+    return target.health;
+  }
+
+  }
---
+++ maestro-objetos/HumanTest.java
@@ -0,0 +1,12 @@
+
+
+public class HumanTest {
+  public static void main(String[] args) {
+    Human prota = new Human();
+    Human enemie = new Human();
+    System.out.println(enemie.health);
+    prota.attack(enemie);
+    System.out.println(enemie.health);
+
+  }
+}
